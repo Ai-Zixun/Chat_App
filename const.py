3b9edceaed163e2998438788a1a015d3770c63b5
Zixun Ai <aiz@uci.edu>
2019-10-18 21:03:14 +0100

    Const

---
+++ const.py
@@ -0,0 +1,7 @@
+DB_HOST = "chat-app-database.c3n2xfnt9l9e.us-east-2.rds.amazonaws.com"
+DB_PORT = 5432
+DB_NAME = "chat_app_database"
+DB_USER = "postgres"
+DB_PASSWORD = "kurosu831107"
+
+JWT_SECRET_KEY = "SECRET_KEY"
